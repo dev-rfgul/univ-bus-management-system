f405383f9551bf66c8b5de0b19ce3e71114d3440
dev-rfgul <rfgul587@gmail.com>
2025-05-14 07:20:41 +0500

    Add busManagement URLs and remove temporary formatting script

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('busManagement.urls')),
     path('bus/', include('busManagement.urls')),
 
 ]
