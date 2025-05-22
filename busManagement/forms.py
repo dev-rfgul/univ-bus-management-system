0cc85bdd4728e3511b3e5ce9473e5a297b15126c
dev-rfgul <rfgul587@gmail.com>
2025-05-22 12:12:26 +0500

    signup and login pages are being displayed

--- busManagement/forms.py
+++ busManagement/forms.py
@@ -4,9 +4,9 @@ from .models import User
 class UserSignupForm(forms.ModelForm):
     class Meta:
         model = User  # 'model' should be lowercase
-        fields = '__all__'  # '__all__' should be a string, but the syntax is correct
+        fields = ('username','email','password',)  # '__all__' should be a string, but the syntax is correct
 
 class UserSignInForm(forms.ModelForm):  # Naming conventions - class name should be PascalCase
     class Meta:
         model = User
-        fields = '__all__'  # Again, '__all__' is correct
+        fields = ('email','password',)  
